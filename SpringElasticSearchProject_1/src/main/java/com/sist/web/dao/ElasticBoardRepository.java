33d83cdf3bc63e82112f34f5d697fb567683509b
chaijewon <vcandjava@nate.com>
2025-05-15 16:48:49 +0900

    1111

--- SpringElasticSearchProject_1/src/main/java/com/sist/web/dao/ElasticBoardRepository.java
+++ SpringElasticSearchProject_1/src/main/java/com/sist/web/dao/ElasticBoardRepository.java
@@ -7,7 +7,7 @@ import com.sist.web.vo.ElasticBoard;
 
 @Repository
 public interface ElasticBoardRepository extends ElasticsearchRepository<ElasticBoard, Integer>{
-   // public ElasticBoard findById(int id);
+   public ElasticBoard findById(int id);
    // CRUD설정 
    
 }
