5d03b105b0723ec8b7151e92b36c5af86cc6b6cb
chaijewon <vcandjava@nate.com>
2025-05-16 13:43:26 +0900

    1111

--- SpringElasticSearchProject_1/src/main/java/com/sist/web/SpringElasticSearchProject1Application.java
+++ SpringElasticSearchProject_1/src/main/java/com/sist/web/SpringElasticSearchProject1Application.java
@@ -2,6 +2,7 @@ package com.sist.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class SpringElasticSearchProject1Application {
 
