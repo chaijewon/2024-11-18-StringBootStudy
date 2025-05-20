1448f4b953c155073b396313e8c281258fc937e1
chaijewon <vcandjava@nate.com>
2025-05-20 12:30:54 +0900

    1111

--- SpringElasticSearchProject_3/src/main/java/com/sist/web/service/BusanInfoService.java
+++ SpringElasticSearchProject_3/src/main/java/com/sist/web/service/BusanInfoService.java
@@ -1,10 +1,21 @@
 package com.sist.web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import com.sist.web.dao.*;
 import com.sist.web.entity.*;
 import java.util.*;
+/*
+ *     Repository : DAO  => 순수하게 데이터베이스 연동 
+ *       
+ *     Service : DAO에서 데이터를 받아서 => 처리후 Controller로 전송 
+ *     Controller : 요청 처리가 완료된 데이터만 받아서 전송
+ *      
+ */
 @Service
 public class BusanInfoService {
    @Autowired
@@ -19,5 +30,40 @@ public class BusanInfoService {
 		   list2.add(list.get(i));
 	   }
 	   return list2;
+   }
+   
+   public List<BusanInfoEntity> getBusanInfoAll(int page)
+   {
+	   int rowSize=12;
+	   Pageable pg=PageRequest.of(page-1, rowSize,Sort.by(Sort.Direction.DESC,"id"));
+	   Page<BusanInfoEntity> pList=bDao.findAll(pg);
+	   
+	   List<BusanInfoEntity> list=new ArrayList<BusanInfoEntity>();
+	   if(pList!=null && pList.hasContent())
+	   {
+		   list=pList.getContent();
+	   }
+	   return list;
+   }
+   
+   public int[] getPageDatas(int page)
+   {
+	   int[] data=new int[4];
+	   int count=(int)bDao.count();
+	   int totalpage=(int)(Math.ceil(count/12.0));
+	   
+	   final int BLOCK=10;
+	   int startPage=((page-1)/BLOCK*BLOCK)+1;
+	   int endPage=((page-1)/BLOCK*BLOCK)+BLOCK;
+	   
+	   if(endPage>totalpage)
+		   endPage=totalpage;
+	   
+	   data[0]=page;
+	   data[1]=totalpage;
+	   data[2]=startPage;
+	   data[3]=endPage;
+	   
+	   return data;
    }
 }
