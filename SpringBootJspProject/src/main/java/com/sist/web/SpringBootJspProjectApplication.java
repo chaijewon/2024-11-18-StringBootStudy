d3e689bbdae3071845dc7145cf20ab2444fe2b70
chaijewon <vcandjava@nate.com>
2025-06-20 11:47:36 +0900

    1111

--- SpringBootJspProject/src/main/java/com/sist/web/SpringBootJspProjectApplication.java
+++ SpringBootJspProject/src/main/java/com/sist/web/SpringBootJspProjectApplication.java
@@ -2,7 +2,8 @@ package com.sist.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// properties에서 자바버전/다이나믹 웹버전 변경(6.0)
+// 스프링 3.5이상에서는 mybatis가 비활성화 된다 
 @SpringBootApplication
 public class SpringBootJspProjectApplication {
 
